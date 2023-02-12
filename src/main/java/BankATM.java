287359324d1c5d7e996d5d60681d48a2bf7d4da5
Gulnara Feygelman <gulya_b@yahoo.com>
2023-02-12 22:42:20 +0300

    1 commit, tests

--- src/main/java/BankATM.java
+++ src/main/java/BankATM.java
@@ -1,3 +1,4 @@
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class BankATM {
@@ -11,9 +12,9 @@ public class BankATM {
             return true;
         } else {
             if (count <= 2) {
-                System.out.println("Извините, пароль неверный, у вас все еще есть " + (3 - count) + " Второй шанс!");
+                System.out.println("Sorry, пароль неверный, у вас все еще есть " + (3 - count) + " Второй шанс!");
             } else {
-                System.out.println("Извините, ваша карта заблокирована!");
+                System.out.println("Sorry, ваша карта заблокирована!");
             }
             return false;
         }
@@ -26,19 +27,81 @@ public class BankATM {
 
         for (int i = 1; i <= 3; i++) {
             System.out.println("Пожалуйста, вставьте свою банковскую карту:");
-            String inputCard = input.next(); // FIXME добавить обработку исключения
-            System.out.println("Пожалуйста, введите ваш пароль:");
-            int inputPwd = input.nextInt(); // FIXME добавить обработку исключения
+            try {
+                String inputCard = input.next();
+                System.out.println("Пожалуйста, введите ваш пароль:");
+                int inputPwd = input.nextInt();
 
-            flag = checkCardAndPassword(inputCard, inputPwd, i);
-            if (flag) {break;}
+                flag = checkCardAndPassword(inputCard, inputPwd, i);
+                if (flag) {
+                    break;
+                }
+            } catch (IllegalStateException | NoSuchElementException e) {
+                System.out.println("Возникла ошибка с " + e.getClass().getCanonicalName());
+                e.printStackTrace();
+                flag = !flag;
+                break;
+            }
         }
         return flag;
     }
 
+    public static void withdrawalOperation(Scanner input, double surplus){
+        // Выполнить операцию снятия
+        double getMoney = input.nextDouble();
+        if (getMoney > 0) {
+            if (getMoney <= surplus) {
+                if (getMoney % 100 == 0) {
+                    System.out.println("Пожалуйста, возьмите свою банкноту! Баланс ￥" + (surplus - getMoney));
+                } else {
+                    System.out.println("Sorry, я не могу принять изменения!");
+                }
+            } else {
+                System.out.println("Sorry, баланс недостаточен!");
+            }
+        } else {
+            System.out.println("Пожалуйста, введите правильную сумму:");
+        }
+    }
+
+
+    public static void depositOperation (Scanner input, double surplus){
+        // выполнить депозитную операцию
+        double saveMoney = input.nextDouble();
+        if (saveMoney > 0 && saveMoney <= 10000) {
+            if (saveMoney % 100 == 0) {
+                surplus += saveMoney;
+                System.out.println("Депозит успешен! Баланс ￥" + surplus);
+            } else {
+
+                double backMoney = saveMoney % 100;
+                surplus = saveMoney + surplus - backMoney;
+                System.out.println("Депозит успешен! Баланс ￥" + surplus);
+                System.out.println("Пожалуйста, заберите сдачу ￥" + backMoney);
+            }
+        } else if (saveMoney > 10000) {
+            System.out.println("Внесите депозит до 10000 юаней за раз, пожалуйста, внесите депозит партиями!");
+        } else {
+            System.out.println("Депонированная банкнота является поддельной банкнотой, и она недействительна и конфискована!");
+        }
+    }
+
+public static void transferOperation(Scanner input, double surplus) {
+    double goMoney = input.nextDouble(); // сумма перевода
+    if (goMoney > 0) {
+        if (goMoney <= surplus) {
+            System.out.println("Перевод успешно! Баланс is" + (surplus - goMoney));
+        } else {
+            System.out.println("Sorry, пожалуйста, убедитесь, что у вас достаточно средств на карте!");
+        }
+
+    } else {
+        System.out.println("Перевод не выполнен! Пожалуйста, введите правильную сумму:");
+    }
+}
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         double surplus = 1000;// баланс
 
         // интерфейс
@@ -55,43 +118,13 @@ public class BankATM {
                         // Выполнить операцию снятия
                         System.out.println("---> Вывод средств");
                         System.out.println("Пожалуйста, введите сумму вывода:");
-                        double getMoney = input.nextDouble();
-                        if (getMoney > 0) {
-                            if (getMoney <= surplus) {
-                                if (getMoney % 100 == 0) {
-                                    System.out.println("Пожалуйста, возьмите свою банкноту! Баланс ￥" + (surplus - getMoney));
-                                } else {
-                                    System.out.println("Извините, я не могу принять изменения!");
-                                }
-                            } else {
-                                System.out.println("Извините, баланс недостаточен!");
-                            }
-                        } else {
-                            System.out.println("Пожалуйста, введите правильную сумму:");
-                        }
-
+                        withdrawalOperation(input, surplus);
                         break;
                     case 2:
                         // выполнить депозитную операцию
                         System.out.println("---> Депозит");
                         System.out.println("Пожалуйста, расположите банкноты и поместите их в депозитный порт:");
-                        double saveMoney = input.nextDouble();
-                        if (saveMoney > 0 && saveMoney <= 10000) {
-                            if (saveMoney % 100 == 0) {
-                                surplus += saveMoney;
-                                System.out.println("Депозит успешен! Баланс ￥" + surplus);
-                            } else {
-
-                                double backMoney = saveMoney % 100;
-                                surplus = saveMoney + surplus - backMoney;
-                                System.out.println("Депозит успешен! Баланс ￥" + surplus);
-                                System.out.println("Пожалуйста, заберите сдачу ￥" + backMoney);
-                            }
-                        } else if (saveMoney > 10000) {
-                            System.out.println("Внесите депозит до 10000 юаней за раз, пожалуйста, внесите депозит партиями!");
-                        } else {
-                            System.out.println("Депонированная банкнота является поддельной банкнотой, и она недействительна и конфискована!");
-                        }
+                        depositOperation(input,surplus);
                         break;
                     case 3:
                         // выполнить запрос баланса
@@ -102,17 +135,7 @@ public class BankATM {
                         // выполнить операцию переноса
                         System.out.println("---> Трансфер");
                         System.out.println("Пожалуйста, введите сумму перевода:");
-                        double goMoney = input.nextDouble(); // сумма перевода
-                        if (goMoney > 0) {
-                            if (goMoney <= surplus) {
-                                System.out.println("Перевод успешно! Баланс is" + (surplus - goMoney));
-                            } else {
-                                System.out.println("Извините, пожалуйста, убедитесь, что у вас достаточно средств на карте!");
-                            }
-
-                        } else {
-                            System.out.println("Перевод не выполнен! Пожалуйста, введите правильную сумму:");
-                        }
+                        transferOperation(input,surplus);
                         break;
                     case 5:
                         // выполнить операцию выхода
@@ -120,7 +143,7 @@ public class BankATM {
                         System.out.println("Спасибо за ваше использование!");
                         return;
                     default:
-                        System.out.println("Извините, выбранная вами функция неверна!");
+                        System.out.println("Sorry, выбранная вами функция неверна!");
                         break;
                 }// switch end
                 System.out.println("Продолжить? Да / Нет");
--- src/test/java/BankAtmTest.java
+++ src/test/java/BankAtmTest.java
@@ -1,22 +1,38 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class BankAtmTest {
 
     //Добавить параметризацию и проверить остальные позитивные и негативные кейсы проверок карты и пароля
-    @Test //Заменить на @ParametrizedTest
-    //@ValueSource(список значений для подстановки)
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
     public void shouldCheckPositiveTest(int count) {
         final boolean res = BankATM.checkCardAndPassword("6228123123", 888888, count);
         Assertions.assertTrue(res);
     }
 
-    @Test
-    public void shouldCheckNegativeTest() {
-        final boolean res = BankATM.checkCardAndPassword("62281231", 888888, 1);
+    @ParameterizedTest
+    @MethodSource("shouldCheckDataProvider")
+    public void shouldCheckNegativeTest(String param1, int param2, int count) {
+        final boolean res = BankATM.checkCardAndPassword(param1, param2, count);
         Assertions.assertFalse(res);
     }
 
+    static Stream<Arguments> shouldCheckDataProvider() {
+        return Stream.of(
+                arguments("62281231", 888888, 1),
+                arguments("6228123123", 888889, 1),
+                arguments("6228123123", 1, 10)
+        );
+    }
     @Test
     public void shouldAuthPositiveTest() {
         final boolean res = BankATM.auth();
